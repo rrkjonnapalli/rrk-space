b673fee0d2e68c1bbfd608c81b6265142efc3720
rrkjonnapalli <r.ravikiranjonnapalli@gmail.com>
2024-02-09 11:57:20 +0530

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
